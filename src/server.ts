162997c99544dd541eec29d8059a1032067faf2d
falco2202 <giaont2001@gmail.com>
2023-01-15 03:47:51 +0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(5000, () => {
+  console.log("Server running");
+})
