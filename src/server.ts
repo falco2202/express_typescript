e81b875d2b0b3a97cd98c7b98147df8327843d03
falco2202 <giaont2001@gmail.com>
2023-01-15 19:07:32 +0700

    update post module

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
-})
-
-app.listen(5000, () => {
-  console.log("Server running");
-})
+import {App} from './app';
+import PostsController from './posts/post.controller';
+ 
+const app = new App(
+  [
+    new PostsController(),
+  ],
+  5000,
+);
+ 
+app.listen();
